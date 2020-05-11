63491eec586b30eb980558f79e473639a0e687bc
helmy-elrais <35847420+helmy-elrais@users.noreply.github.com>
2020-05-11 19:34:25 +0200

    Definition of the Fine Tuned Bert Model

---
+++ BertFineTuned.py
@@ -0,0 +1,36 @@
+import torch
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import re
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data.sampler import SubsetRandomSampler
+import transformers
+from transformers import RobertaTokenizer, BertTokenizer, RobertaModel, BertModel, AdamW# get_linear_schedule_with_warmup
+from transformers import get_linear_schedule_with_warmup
+import time
+
+
+class BertConsumerComplaints(nn.Module):
+    def __init__(self):
+        super(BertConsumerComplaints, self).__init__()
+        self.bert_path='bert-base-uncased'
+        self.bert=transformers.BertModel.from_pretrained(self.bert_path)
+        #self.bert_drop=nn.Dropout(0.2)
+        #self.fc=nn.Linear(768,256)
+        #self.out=nn.Linear(256,10)
+        self.out=nn.Linear(768,10)
+        #self.relu=nn.ReLU()
+    
+    def forward(self, ids, mask, token_type_ids):
+        _,pooled_out= self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        #rh=self.bert_drop(pooled_out)
+        #rh=self.fc(rh)
+        #rh=self.relu(rh)
+        return self.out(pooled_out)

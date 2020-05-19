fc3a4d4522b16f692c0a05666a633af7f6b2b044
helmy-elrais <35847420+helmy-elrais@users.noreply.github.com>
2020-05-19 05:15:12 +0200

    update of utils.py

--- utils.py
+++ utils.py
@@ -17,105 +17,31 @@ from transformers import get_linear_schedule_with_warmup
 import time
 
 
+
+def my_collate1(batches):
+    #return batches
+    return [{key:torch.stack(value) for key, value in batch.items()} for batch in batches]
+
+
 def loss_fun(outputs, targets):
     loss= nn.CrossEntropyLoss()
     return loss(outputs, targets)
-    #return nn.BCEWithLogitsLoss()(outputs, targets)   
+    #return nn.BCEWithLogitsLoss()(outputs, targets)
+    
 
 
-def train_loop_fun(data_loader, model, optimizer, device, scheduler=None):
-    model.train()
-    t0 = time.time()
-    losses=[]
-    for batch_idx, data in enumerate(data_loader):
-        #for datapoint in data:
-        for sentence_tokens in data:
-            for chunk_tokens in sentence_tokens:
-            
-    #             model.half()
-
-                ids=chunk_tokens["ids"]
-                mask=chunk_tokens["mask"]
-                token_type_ids = chunk_tokens["token_type_ids"]
-                targets = chunk_tokens["targets"]
-                
-                ids=torch.unsqueeze(ids,dim=0)
-                mask=torch.unsqueeze(mask, dim=0)
-                token_type_ids=torch.unsqueeze(token_type_ids, dim=0)
-                targets=torch.unsqueeze(targets, dim=0)
-                
-    #         for i in range(len(data["targets"])):
-    #             ids=torch.cat(data["ids"])
-    #             mask=torch.cat(data["mask"])
-    #             token_type_ids = torch.cat(data["token_type_ids"])
-    #             targets = torch.cat(data["targets"])
-                #print(targets)
-
-    #             ids=data["ids"][i]
-    #             mask=data["mask"][i]
-    #             token_type_ids = data["token_type_ids"][i]
-    #             targets = data["targets"][i]
-
-                ids = ids.to(device, dtype=torch.long)
-                mask = mask.to(device, dtype=torch.long)
-                token_type_ids = token_type_ids.to(device, dtype=torch.long)
-                targets = targets.to(device, dtype=torch.long)
-
-                optimizer.zero_grad()
-                outputs=model(ids=ids, mask=mask, token_type_ids=token_type_ids)
-                loss=loss_fun(outputs, targets)
-                loss.backward()
-                model.float()
-                optimizer.step()
-                if scheduler:
-                    scheduler.step()
-                losses.append(loss.item())
-        if batch_idx % 10 == 0:
-            print(f"___ batch index = {batch_idx} / {len(data_loader)} ({100*batch_idx / len(data_loader):.2f}%), loss = {np.mean(losses[-10:]):.4f}, time = {time.time()-t0:.2f} secondes ___")
-            t0 = time.time()
-    return losses
-
-
-def eval_loop_fun(data_loader, model, device):
-    model.eval()
-    fin_targets=[]
-    fin_outputs=[]
-    losses=[]
-    for batch_idx, data in enumerate(data_loader):
-        
-#         for i in range(len(data["targets"])):
-        
-        for sentence_tokens in data:
-            for chunk_tokens in sentence_tokens:
-                ids=chunk_tokens["ids"]
-                mask=chunk_tokens["mask"]
-                token_type_ids = chunk_tokens["token_type_ids"]
-                targets = chunk_tokens["targets"]
-
-                ids=torch.unsqueeze(ids,dim=0)
-                mask=torch.unsqueeze(mask, dim=0)
-                token_type_ids=torch.unsqueeze(token_type_ids, dim=0)
-                targets=torch.unsqueeze(targets, dim=0)
-                
-    #             ids=data["ids"][i]
-    #             mask=data["mask"][i]
-    #             token_type_ids = data["token_type_ids"][i]
-    #             targets = data["targets"][i]
-                #print(targets)
-
-                ids = ids.to(device, dtype=torch.long)
-                mask = mask.to(device, dtype=torch.long)
-                token_type_ids = token_type_ids.to(device, dtype=torch.long)
-                targets = targets.to(device, dtype=torch.long)
-                with torch.no_grad():
-                    outputs=model(ids=ids, mask=mask, token_type_ids=token_type_ids)
-                    loss=loss_fun(outputs, targets)
-                    losses.append(loss.item())
-
-                fin_targets.append(targets.cpu().detach().numpy())
-                fin_outputs.append(torch.softmax(outputs, dim=1).cpu().detach().numpy())
-    return np.vstack(fin_outputs), np.vstack(fin_targets), losses
-
+def evaluate(target, predicted):
+    true_label_mask=[1 if (np.argmax(x)-target[i])==0 else 0 for i,x in enumerate(predicted)]
+    nb_prediction=len(true_label_mask)
+    true_prediction=sum(true_label_mask)
+    false_prediction=nb_prediction-true_prediction
+    accuracy= true_prediction/nb_prediction
+    return{
+        "accuracy":accuracy,
+        "nb exemple":len(target),
+        "true_prediction":true_prediction,
+        "false_prediction":false_prediction,
+    }
 
 
 def train_loop_fun1(data_loader, model, optimizer, device, scheduler=None):
@@ -154,7 +80,7 @@ def train_loop_fun1(data_loader, model, optimizer, device, scheduler=None):
         mask = mask.to(device, dtype=torch.long)
         token_type_ids = token_type_ids.to(device, dtype=torch.long)
         targets = targets.to(device, dtype=torch.long)
-
+        
         optimizer.zero_grad()
         outputs=model(ids=ids, mask=mask, token_type_ids=token_type_ids)
         loss=loss_fun(outputs, targets)
@@ -164,7 +90,7 @@ def train_loop_fun1(data_loader, model, optimizer, device, scheduler=None):
         if scheduler:
             scheduler.step()
         losses.append(loss.item())
-        if batch_idx % 10 == 0:
+        if batch_idx % 250 == 0:
             print(f"___ batch index = {batch_idx} / {len(data_loader)} ({100*batch_idx / len(data_loader):.2f}%), loss = {np.mean(losses[-10:]):.4f}, time = {time.time()-t0:.2f} secondes ___")
             t0 = time.time()
     return losses
@@ -221,36 +147,6 @@ def eval_loop_fun1(data_loader, model, device):
 
 
 
-
-
-def my_collate(batch):
-    #print("$$$$$$$$$$$$$$$",batch)
-#     data = [item[0] for item in batch]
-#     target = [item[1] for item in batch]
-    #target = torch.LongTensor(target)
-    return batch
-
-
-def my_collate1(batches):
-    #return batches
-    return [{key:torch.stack(value) for key, value in batch.items()} for batch in batches]
-
-
-
-def evaluate(target, predicted):
-    true_label_mask=[1 if (np.argmax(x)-target[i])==0 else 0 for i,x in enumerate(predicted)]
-    nb_prediction=len(true_label_mask)
-    true_prediction=sum(true_label_mask)
-    false_prediction=nb_prediction-true_prediction
-    accuracy= true_prediction/nb_prediction
-    return{
-        "accuracy":accuracy,
-        "nb exemple":len(target),
-        "true_prediction":true_prediction,
-        "false_prediction":false_prediction,
-    }
-
-
 def rnn_train_loop_fun1(data_loader, model, optimizer, device, scheduler=None):
     model.train()
     t0 = time.time()
@@ -287,7 +183,6 @@ def rnn_train_loop_fun1(data_loader, model, optimizer, device, scheduler=None):
         mask = mask.to(device, dtype=torch.long)
         token_type_ids = token_type_ids.to(device, dtype=torch.long)
         targets = targets.to(device, dtype=torch.long)
-
         optimizer.zero_grad()
         outputs=model(ids=ids, mask=mask, token_type_ids=token_type_ids, lengt=lengt)
         loss=loss_fun(outputs, targets)
@@ -297,7 +192,7 @@ def rnn_train_loop_fun1(data_loader, model, optimizer, device, scheduler=None):
         if scheduler:
             scheduler.step()
         losses.append(loss.item())
-        if batch_idx % 10 == 0:
+        if batch_idx % 640 == 0:
             print(f"___ batch index = {batch_idx} / {len(data_loader)} ({100*batch_idx / len(data_loader):.2f}%), loss = {np.mean(losses[-10:]):.4f}, time = {time.time()-t0:.2f} secondes ___")
             t0 = time.time()
     return losses

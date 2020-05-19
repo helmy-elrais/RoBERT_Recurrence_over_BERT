64a37b7685869f65fedaa9b9f5b0c92d48747063
helmy-elrais <35847420+helmy-elrais@users.noreply.github.com>
2020-05-19 06:10:05 +0200

    Definition of the BERT Hierarchical Model

---
+++ BERT_Hierarchical.py
@@ -0,0 +1,43 @@
+import torch
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import re
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data.sampler import SubsetRandomSampler
+import transformers
+from transformers import RobertaTokenizer, BertTokenizer, RobertaModel, BertModel, AdamW# get_linear_schedule_with_warmup
+from transformers import get_linear_schedule_with_warmup
+import time
+
+
+class BERT_Hierarchical_Model(nn.Module):
+    
+    def __init__(self, pooling_method="mean"):
+        super(BERT_Hierarchical_Model, self).__init__()
+        
+        self.pooling_method=pooling_method
+        
+        self.bert_path='bert-base-uncased'
+        self.bert=transformers.BertModel.from_pretrained(self.bert_path)
+        self.out=nn.Linear(768,10)
+    
+    
+    def forward(self, ids, mask, token_type_ids, lengt):
+        
+        _,pooled_out= self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        
+        chunks_emb=pooled_out.split_with_sizes(lengt)
+        
+        if self.pooling_method=="mean":
+            emb_pool=torch.stack([torch.mean(x, 0) for x in chunks_emb])
+        elif self.pooling_method=="max":
+            emb_pool=torch.stack([torch.max(x, 0)[0] for x in chunks_emb])
+        
+        return self.out(emb_pool)
